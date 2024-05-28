a2a226fb1ae57e4037a7d57a0aaa6407733bdd93
sebulo <sebastianloeschcke@gmail.com>
2024-05-28 18:28:17 +0200

    13b continued pretraining

--- scripts/continued_pretraining/llama2_13b_ic3.sh
+++ scripts/continued_pretraining/llama2_13b_ic3.sh
@@ -7,7 +7,7 @@ torchrun --standalone --nproc_per_node 1 torchrun_main.py \
     --galore_scale 0.25 \
     --rank 1024 \
     --update_proj_gap 10000 \
-    --batch_size 8 \
+    --batch_size 1 \
     --total_batch_size 512 \
     --num_training_steps 60000 \
     --warmup_steps 6000 \
@@ -20,10 +20,10 @@ torchrun --standalone --nproc_per_node 1 torchrun_main.py \
     --quantize_projection_matrix '4bit' \
     --seed 42 \
     --save_every 1000 \
-    --save_dir checkpoints/llama2_7b_ic3 \
+    --save_dir checkpoints/llama2_13b_ic3 \
     --proj_gap_progression "exponential" \
     --increment_size 1.15 \
-    --name llam2_7b_1.15_100_icelandic \
+    --name llam2_13b_1.15_100_icelandic \
     --use_offloading True \
     --use_double_quant True \
     --use_loqt True \
