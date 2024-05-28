0e92140a606c9bf996845b48b6c275e39d2d194b
sebulo <sebastianloeschcke@gmail.com>
2024-05-28 15:16:52 +0200

    13b finetuning

---
+++ scripts/continued_pretraining/llama2_13b_ic3.sh
@@ -0,0 +1,30 @@
+# tested on 3090 w bsz 1, 4, 8, nvidia driver 535 
+torchrun --standalone --nproc_per_node 1 torchrun_main.py \
+    --model_name meta-llama/Llama-2-13b-hf \
+    --dataset_name mideind/icelandic-common-crawl-corpus-IC3 \
+    --use_hf_model \
+    --lr 0.0001 \
+    --galore_scale 0.25 \
+    --rank 1024 \
+    --update_proj_gap 10000 \
+    --batch_size 8 \
+    --total_batch_size 512 \
+    --num_training_steps 60000 \
+    --warmup_steps 6000 \
+    --dtype bfloat16 \
+    --eval_every 2000 \
+    --optimizer adam8bit \
+    --lora_alpha 0.5 \
+    --bnb_4bit_quant_type nf4 \
+    --quantize_w '4bit' \
+    --quantize_projection_matrix '4bit' \
+    --seed 42 \
+    --save_every 1000 \
+    --save_dir checkpoints/llama2_7b_ic3 \
+    --proj_gap_progression "exponential" \
+    --increment_size 1.15 \
+    --name llam2_7b_1.15_100_icelandic \
+    --use_offloading True \
+    --use_double_quant True \
+    --use_loqt True \
+    --single_gpu 
