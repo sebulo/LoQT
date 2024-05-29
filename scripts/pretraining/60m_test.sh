824d15d32d39a8ff8940098faf1d6df67ed0414f
sloeschcke <sebastianloeschcke@gmail.com>
2024-05-29 17:32:08 +0200

    added test for checking if logits are the same after merging and dequantizing

---
+++ scripts/pretraining/60m_test.sh
@@ -0,0 +1,25 @@
+# --nproc_per_node 1 is number of GPUs per node
+torchrun --standalone --nproc_per_node 1 --nnodes 1 torchrun_main.py \
+    --model_config configs/llama_60m.json \
+    --seed 42 \
+    --lr 0.01 \
+    --rank 128 \
+    --lora_alpha 0.4 \
+    --update_proj_gap 50 \
+    --batch_size 512 \
+    --total_batch_size 512 \
+    --num_training_steps 100 \
+    --warmup_steps 10 \
+    --eval_every 30 \
+    --save_every 60 \
+    --dtype bfloat16 \
+    --optimizer adamw \
+    --use_loqt True\
+    --quantize_w '4bit' \
+    --quantize_projection_matrix '4bit' \
+    --compensate_quant_error_iterations 5 \
+    --proj_gap_progression "exponential" \
+    --increment_size 1.2 \
+    --single_gpu \
+    --name 60m_LoQT
+    #--continue_from 'checkpoints/60m_LoQT_1716992064/latest_checkpoint' \
