6ec7d470cb80cbc22629355d44043934fe349983
Mads <mabeto5p@live.dk>
2024-08-04 12:40:53 +0000

    progress

---
+++ scripts/finetune_gsm8k/gsm8k_7B.sh
@@ -0,0 +1,22 @@
+# LoQT COLA r=32
+python run_GSM8K.py \
+  --model_name_or_path meta-llama/Llama-2-7b-hf \
+  --task_name gsm8k \
+  --num_train_epochs 20 \
+  --seed 9876 \
+  --lora_r 32 \
+  --lora_alpha 2 \
+  --update_proj_gap 2400 \
+  --bnb_4bit_quant_type nf4 \
+  --quantize_w '4bit' \
+  --quantize_projection_matrix '4bit' \
+  --compensate_quant_error_iterations 5 \
+  --max_length 256 \
+  --pad_to_max_length \
+  --per_device_train_batch_size 32 \
+  --learning_rate 8e-5 \
+  --output_dir checkpoints \
+  --use_loqt true \
+  --single_gpu \
+  --with_tracking \
+  --report_to wandb 
